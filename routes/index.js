2b58c235c269fba50b56b5b89d2f209e28cf165f
Matt Potter <mattpotter28@gmail.com>
2017-11-04 17:13:43 -0500

    blank mybartendr

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'mybartendr' });
+  res.render('index', { name: 'mybartendr' });
 });
 
 module.exports = router;
