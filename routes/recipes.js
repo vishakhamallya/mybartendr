da4749f423a77cc9b6b33bd84ab95ae58017ad78
mehmoodq <mehmood.qadri21@gmail.com>
2017-11-04 19:45:11 -0500

    test

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET recipes. */
 router.get('/', function(req, res, next) {
-  res.render('index', { name: 'mybartendr' });
+    res.render('recipes', { name: 'recipes' });
 });
 
 module.exports = router;
