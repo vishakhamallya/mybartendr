3a076d9ff31ed955e064626e1c5de00050384633
Matt Potter <mattpotter28@gmail.com>
2017-11-04 21:29:19 -0500

    darkly theming

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,12 @@ router.get('/recipes', function(req, res, next) {
     res.render('recipes.jade', { name: 'recipes' });
 });
 
+router.get('/pairings', function(req, res, next) {
+    res.render('pairings.jade', { name: 'pairings' });
+});
+
+router.get('/login', function(req, res, next) {
+    res.render('login.jade', { name: 'login' });
+});
+
 module.exports = router;
--- routes/recipes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET recipes. */
-router.get('/', function(req, res, next) {
-    res.render('recipes', { name: 'recipes' });
-});
-
-module.exports = router;
