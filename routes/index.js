83e82a8e15a1df4785292464142aab38964bfbc9
mehmoodq <mehmood.qadri21@gmail.com>
2017-11-04 20:13:00 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { name: 'mybartendr' });
+  res.render('index.jade', { name: 'mybartendr' });
 });
 
+router.get('/recipes', function(req, res, next) {
+    res.render('recipes.jade', { name: 'recipes' });
+});
 
 module.exports = router;
