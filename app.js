9deb0d40082ae916b2b157cbd4e119b1e42a8484
Matt Potter <mattpotter28@gmail.com>
2017-11-05 01:55:26 -0500

    MySQL connection

--- app.js
+++ app.js
@@ -55,6 +55,11 @@ var con = mysql.createConnection({
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
+
+    con.query("SELECT * FROM users", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+    });
 });
 
 
