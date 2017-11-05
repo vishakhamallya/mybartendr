035aa0472595bb3814f478e1e17ad0e076feb530
Matt Potter <mattpotter28@gmail.com>
2017-11-04 20:07:47 -0500

    login right align; fix routing

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { name: 'mybartendr' });
+router.get('/', function(req, res, next){
+    res.render('index.jade', { name: 'mybartndr' });
 });
 
+router.get('/recipes', function(req, res){
+    res.render('recipes.jade', { title: 'recipes' });
+});
 
 module.exports = router;
